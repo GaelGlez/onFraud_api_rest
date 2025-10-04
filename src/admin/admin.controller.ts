c80dd77cdefaf6dd6013bbfdfcf61a31e6348594
GaelGlez <gaelos.glez@gmail.com>
2025-10-03 20:58:32 -0600

    Endpoint GET /admin/reports

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,12 +1,13 @@
-import { Body, Controller, Put, Get, Param  } from "@nestjs/common";
+import { Body, Controller, Put, Get, Param, Query  } from "@nestjs/common";
 import { UserService } from "../users/users.service";
 import { ApiOperation, ApiTags } from "@nestjs/swagger";
 import { UpdateUserDto } from "../users/dto/users.dto";
+import { ReportsService } from "../reports/reports.service";
 
 @ApiTags('Modulo de Administracion') // Agrupa los endpoints de este controlador bajo el tag "Modulo de Administracion"
 @Controller('admin')
 export class AdminController {
-    constructor(private readonly userService: UserService) {}
+    constructor(private readonly userService: UserService, private readonly reportsService: ReportsService) {}
 
     @ApiOperation({summary: 'Buscar usuario por ID'}) // Descripción de la operación para Swagger
     @Get('users/:id')
@@ -40,6 +41,17 @@ export class AdminController {
             full_name: updateUser.full_name,
             email: updateUser.email
         };
+    }
+
+    //Reportes
+    @Get('reports')
+    async getReports(
+        @Query('category_id') categoryId?: number,
+        @Query('status_id') statusId?: number,
+        @Query('url') url?: string,
+        @Query('q') keyword?: string) {
+        const reports = await this.reportsService.findAllReports({ categoryId, statusId, url, keyword });
+        return reports;
     }
 }
 
--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -2,10 +2,12 @@ import { Module } from "@nestjs/common";
 import { AdminController } from "./admin.controller";
 import { UserService } from "../users/users.service";
 import { UsersRepository } from "../users/users.repository";
+import { ReportsService } from "../reports/reports.service";
+import { ReportsRepository } from "src/reports/reports.repository";
 
 @Module({
     controllers: [AdminController],
-    providers: [UserService, UsersRepository],
+    providers: [UserService, UsersRepository, ReportsService, ReportsRepository],
     //exports: [UserService],
 })
 export class AdminModule {}
