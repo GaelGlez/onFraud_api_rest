4d37e6886e0aacb3cf8a3661a279fb2c65c5c8a1
Carlos Olivarez <carmanolmon@gmail.com>
2025-10-22 19:08:54 -0600

    db.service datos BD

--- src/db/db.service.ts
+++ src/db/db.service.ts
@@ -10,7 +10,7 @@ export class DbService implements OnModuleInit, OnModuleDestroy {
             port: 3306, // No cambiar
             host: process.env.HOST || 'localhost', // Dirección de tu servidor MySQL (si es local, déjalo como está)
             user: process.env.USER || 'root', // El usuario de tu base de datos (en caso de que no sea root, cámbialo)
-            password: process.env.PASSWORD || 'Fridayabi2005', // Cambia esto por la contraseña de tu usuario root
+            password: process.env.PASSWORD || 'w2eRn9P$qlY%', // Cambia esto por la contraseña de tu usuario root
             database: process.env.DATABASE || 'onfraud', // Nombre de la base de datos MySQL
         });
     }
